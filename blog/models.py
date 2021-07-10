619b41c3b4372c9fab5e277b35a35f8c0df916a7
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2021-07-10 20:44:03 +0430

    create model
    create model and admin and blog and contact

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
+class post(models.Model):
+    #image
+    title = models.CharField(_("عنوان"),max_length=255)
+    content = models.TextField(_("مطالب"))
+    #tag
+    #category
+    countent_views = models.IntegerField(_("تعداد بازدید"),default=0)
+    status = models.BooleanField(_("وضعیت"),default=False) 
+    created_date = models.DateTimeField(_("زمان تولید"), auto_now_add=True)
+    updated_date = models.DateTimeField(_("تاریخ بروز رسانی"), auto_now=True)
+    published_date = models.DateTimeField(_("زمان انتشار"),null=False)
+    
+    def __str__(self):
+        return self.name
+    
--- mysit/models.py
+++ mysit/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class contact(models.Model):
+    neme = models.CharField(_("نام"), max_length=50)
+    email = models.EmailField(_("پست الکترونیکی"), max_length=254)
+    subject = models.CharField(_("موضوع"), max_length=100)
+    massage = models.TextField(_("پیام"))
+    created_date = models.DateTimeField(_("زمان تولید"), auto_now=False, auto_now_add=True)
+    updated_date = models.DateTimeField(_("تاریخ بروز رسانی"), auto_now=True, auto_now_add=False)
+    
+    def __str__(self):
+        return self.name
+    
