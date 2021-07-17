4f83ef002a309c2045eea8c88742ac933d5b6436
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2021-07-17 20:36:58 +0430

    Paginator
    a create table with pagination and Django view pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def blog_views(request,cat_name=None):
     posts= post.objects.filter(status=1)
     if cat_name:
         posts=posts.filter(category__name=cat_name)
+    
+    posts = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    try:
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     contex={'posts':posts}
     return render(request,'blog/blog.html',contex)
 
